680318227bdb6a12bc517d73b40163b5023c2a85
Chris Tonkinson <chris@tonkinson.com>
2026-05-24 17:37:14 -0400

    Refactor tests

--- tests/minimal-init.lua
+++ tests/minimal-init.lua
@@ -4,7 +4,10 @@ vim.opt.runtimepath:prepend(vim.fn.getcwd() .. "/.deps/mini.nvim")
 require("mini.test").setup({
   collect = {
     find_files = function()
-      return { "tests/test-map-list.lua" }
+      return {
+        "tests/test-map-list.lua",
+        "tests/test-source-providers.lua",
+      }
     end,
   },
 })
